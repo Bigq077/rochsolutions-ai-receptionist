42b9e58b123b2ae40e57327fc3e94969570f24d9
Bigq077 <quentinroch10@gmail.com>
2025-12-25 22:18:05 +0000

    Update config.py

--- app/central/config.py
+++ app/central/config.py
@@ -1,1 +1,12 @@
+import os
+
+BASE_URL = os.getenv("BASE_URL", "")
+ENV = os.getenv("ENV", "dev")
+
+# These will be used later (safe to be empty now)
+DATABASE_URL = os.getenv("DATABASE_URL")
+REDIS_URL = os.getenv("REDIS_URL")
+
+GOOGLE_SERVICE_ACCOUNT_JSON = os.getenv("GOOGLE_SERVICE_ACCOUNT_JSON")
+DEFAULT_CALENDAR_ID = os.getenv("DEFAULT_CALENDAR_ID")
 
