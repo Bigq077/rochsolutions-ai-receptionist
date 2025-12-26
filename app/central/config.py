045c103e87411f45211689947fa7742b75aa4e66
Bigq077 <quentinroch10@gmail.com>
2025-12-26 10:08:11 +0000

    Delete app/central directory

--- app/central/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-BASE_URL = os.getenv("BASE_URL", "").rstrip("/")
-ENV = os.getenv("ENV", "dev")
-
-REDIS_URL = os.getenv("REDIS_URL")
-
-DEFAULT_CALENDAR_ID = os.getenv("DEFAULT_CALENDAR_ID")
-GOOGLE_SERVICE_ACCOUNT_JSON = os.getenv("GOOGLE_SERVICE_ACCOUNT_JSON")
