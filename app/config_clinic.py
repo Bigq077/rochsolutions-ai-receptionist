5e776e0afda46f3e649858d9f957b4f23978a937
Bigq077 <quentinroch10@gmail.com>
2025-12-29 16:44:23 +0000

    Update config_clinic.py

--- app/config_clinic.py
+++ app/config_clinic.py
@@ -1,1 +1,19 @@
+CLINIC = {
+    "name": "Roch Solutions Clinic",
+    "timezone": "Europe/London",
+    "hours": {
+        "mon_fri": "09:00-18:00",
+        "sat": "Closed",
+        "sun": "Closed",
+    },
+    "location": {
+        "address": "ADD YOUR ADDRESS HERE",
+        "parking": "ADD PARKING INFO HERE",
+    },
+    "prices": {
+        "initial_assessment": "£XX",
+        "follow_up": "£XX",
+    },
+    "appointment_durations": [30, 45, 60],
+}
 
